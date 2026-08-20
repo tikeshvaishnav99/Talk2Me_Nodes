e82a47a053b4869497d60887b0edc5a9963d1627
tikeshvaishnav99 <tikeshvaishnav99@gmail.com>
2026-08-20 22:30:42 +0530

    index.js

--- index.js
+++ index.js
@@ -449,4 +449,6 @@ setInterval(() => {
 }, 3000);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`[Talk2Me Engine] Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`[Talk2Me Engine] Server running on port ${PORT}`);
+});
